38d6f726db766d1e525255961da690b686cc01b9
nicolas.woerner <nicolaswoerner@MBP-von-Nicolas.fritz.box>
2024-04-08 09:01:28 +0200

    improve error handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse, StreamingResponse
 import os
 import re
@@ -8,18 +8,16 @@ from openai._streaming import Stream
 from openai.types.chat import ChatCompletionChunk
 from typing import AsyncIterable
 
-debug = os.environ.get("GPTSCRIPT_DEBUG", "false") == "true"
-
-app = FastAPI()
-
-url = "https://api.perplexity.ai/chat/completions"
-
 if "PERPLEXITY_API_KEY" in os.environ:
     api_key = os.environ["PERPLEXITY_API_KEY"]
 else:
     raise SystemExit("PERPLEXITY_API_KEY not found in environment variables")
 
+debug = os.environ.get("GPTSCRIPT_DEBUG", "false") == "true"
+
 client = OpenAI(api_key=api_key, base_url="https://api.perplexity.ai")
+
+app = FastAPI()
 
 def log(*args):
     if debug:
@@ -41,42 +39,37 @@ async def list_models() -> JSONResponse:
 
 @app.post("/v1/chat/completions")
 async def completions(request: Request) -> StreamingResponse:
-    data = await request.body()
-    data = json.loads(data)
-
     try:
-        tools = data["tools"]
-    except Exception as e:
-        log("No tools provided: ", e)
-        tools = None
+        data = await request.json()
+        model = data.get("model")
+        messages = data.get("messages")
 
-    model = data["model"]
+        if model is None:
+            raise HTTPException(status_code=400, detail="Unable to identify a model.")
+        if messages is None:
+            raise HTTPException(status_code=400, detail="Unable to identify messages.")
 
-    try:
-        messages = data["messages"]
-        for message in messages:
-            if 'content' in message.keys() and message["content"].startswith("[TOOL_CALLS] "):
-                message["content"] = ""
+        try:
+            res = client.chat.completions.create(
+                model=model,
+                messages=messages,
+                stream=True
+            )
+        except Exception as e:
+            log("Error getting a response from Perplexity.", e)
+            raise HTTPException(status_code=500, detail="Error getting a response from Perplexity.")
 
-            if 'tool_call_id' in message.keys():
-                message["name"] = re.sub(r'^call_(.*)_\d$', r'\1', message["tool_call_id"])
-    except Exception as e:
-        log("an error happened mapping tool_calls/tool_call_ids: ", e)
-        messages = None
+        return StreamingResponse(convert_stream(res), media_type="application/x-ndjson")
     
-    res = client.chat.completions.create(
-        model="sonar-small-chat",
-        messages=messages,
-        stream=True
-    )
-
-    return StreamingResponse(convert_stream(res), media_type="application/x-ndjson")
+    except Exception as e:
+        log("An unexpected error occurred.", e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 async def convert_stream(stream: Stream[ChatCompletionChunk]) -> AsyncIterable[str]:
     for chunk in stream:
-        log("CHUNK: ", chunk.json())
-        yield "data: " + str(chunk.json()) + "\n\n"
-
+        log("CHUNK: ", chunk.model_dump_json())
+        yield "data: " + str(chunk.model_dump_json()) + "\n\n"
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=int(os.environ.get("PORT", "8000")),
