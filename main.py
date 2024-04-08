fe7237707d649ac820e84cfa285c25e06b4eb3ae
nicolas.woerner <nicolaswoerner@MBP-von-Nicolas.fritz.box>
2024-04-08 07:42:07 +0200

    add chat completions route

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import json
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse, StreamingResponse
 import os
+from openai import OpenAI
+import requests
 
 debug = os.environ.get("GPTSCRIPT_DEBUG", "false") == "true"
 
 app = FastAPI()
 
+url = "https://api.perplexity.ai/chat/completions"
 
+if "PERPLEXITY_API_KEY" in os.environ:
+    api_key = os.environ["PERPLEXITY_API_KEY"]
+else:
+    raise SystemExit("PERPLEXITY_API_KEY not found in environment variables")
 
 @app.get("/")
 async def get_root():
     return "ok"
-
 
 # The perplexity API does not have a method to list models, so we are hard coding the models here
 @app.get("/v1/models")
@@ -25,8 +31,40 @@ async def list_models() -> JSONResponse:
         {"id": "sonar-medium-online", "name": "Perplexity Sonar Medium Online"}
     ]})
 
-# @app.post("/v1/chat/completions")
-# async def completions(request: Request) -> StreamingResponse:
+@app.post("/v1/chat/completions")
+async def completions(request: Request) -> StreamingResponse:
+    try:
+        payload = {
+            "model": "sonar-small-chat",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "Be precise and concise. Only print maximum 5 words."
+                },
+                {
+                    "role": "user",
+                    "content": "How many stars are there in our galaxy?"
+                }
+            ]
+        }
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError if the response code is not 2xx
+
+        print(response.text)
+        return StreamingResponse(content=response.text, media_type="application/json")
+
+    except requests.RequestException as e:
+        # Handles exceptions raised by the requests library
+        error_message = e.response.text if hasattr(e, "response") and e.response else str(e)
+        raise HTTPException(status_code=500, detail="Internal server error occurred. Error message: " + error_message + request)
+
 
 if __name__ == "__main__":
     import uvicorn
